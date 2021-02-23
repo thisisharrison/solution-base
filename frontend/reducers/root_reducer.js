106d984f5345b0408060ec8622b17720e642c64b
thisisharrison <harrison.lau@outlook.com>
2021-02-23 22:03:28 +0800

    Root set up

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,12 @@
+import { combineReducers } from "redux";
+
+// const rootReducer = combineReducers({
+
+// });
+
+const rootReducer = (state = {}, action) => {
+  return state;
+}
+
+export default rootReducer;
+
