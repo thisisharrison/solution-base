16acd58bea46cac5643c79d15bc58e0d44adb831
thisisharrison <harrison.lau@outlook.com>
2021-03-01 17:59:05 +0800

    Render Count 0

--- frontend/components/topics/topic_show.jsx
+++ frontend/components/topics/topic_show.jsx
@@ -12,13 +12,13 @@ export default function TopicShow({ topicId, topic, posts, problems, solutions, 
       <h2>Topic</h2>
       <pre>{JSON.stringify(topic.topic, undefined, 2)}</pre>
       <h2>Problems</h2>
-      <small>Count: {Object.keys(problems).length}</small>
+      <small>Count: {problems ? Object.keys(problems).length : '0'}</small>
       <pre>{JSON.stringify(problems, undefined, 2)}</pre>
       <h2>Solutions</h2>
-      <small>Count: {Object.keys(solutions).length}</small>
+      <small>Count: {solutions ? Object.keys(solutions).length : '0'}</small>
       <pre>{JSON.stringify(solutions, undefined, 2)}</pre>
       <h2>All Posts</h2>
-      <small>Count: {Object.keys(posts).length}</small>
+      <small>Count: {posts ? Object.keys(posts).length : '0'}</small>
       <pre>{JSON.stringify(posts, undefined, 2)}</pre>
     </div>
   )
