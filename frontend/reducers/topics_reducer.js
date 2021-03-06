68c563755d144f5ca8f247cd5c3440c776c01a86
thisisharrison <harrison.lau@outlook.com>
2021-03-06 14:02:27 +0800

    Bookmark works for Topic and Post

--- frontend/reducers/topics_reducer.js
+++ frontend/reducers/topics_reducer.js
@@ -1,13 +1,31 @@
 import {
   RECEIVE_TOPIC
 } from '../actions/topic_actions';
+import {
+  RECEIVE_POST
+} from '../actions/post_actions';
 
 const topicsReducer = (state = {}, action) => {
   Object.freeze(state);
+  let newState = Object.assign({}, state);
   switch (action.type) {
     case RECEIVE_TOPIC:
       let topic = { [action.topic.topic.id] : action.topic }
       return Object.assign({}, state, topic);
+
+    case RECEIVE_POST:
+      // check post topics
+      const topicIds = action.post.topics.map(topic => topic.id);
+      // check problem or solution, make it plural for topics state
+      const postType = action.post.post_type + 's';
+      // loop through topics in problem/solution and update
+      topicIds.forEach(id => {
+        if (newState[id]) {
+          newState[id][postType] = {...newState[id][postType], [action.post.id] : action.post }
+        }
+      })
+      return newState;
+
     default:
       return state;
   }
