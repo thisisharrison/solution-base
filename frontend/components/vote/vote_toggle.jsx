28d9a4d45ad62de7641bc38e24a605375097a7a5
thisisharrison <harrison.lau@outlook.com>
2021-03-17 18:12:20 +0800

    Vote icon

--- frontend/components/vote/vote_toggle.jsx
+++ frontend/components/vote/vote_toggle.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react'
 import { Button } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import { vote, unvote } from '../../actions/vote_actions';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';;
 
 export default function VoteToggle({ hasVoted, id, type, votes }) {
   const [ _vote, setVote ] = useState(() => hasVoted || false);
@@ -17,6 +19,16 @@ export default function VoteToggle({ hasVoted, id, type, votes }) {
   };
 
   return (
-    <Button variant={_vote ? "outline-primary" : "primary"} onClick={updateVote}>{_vote ? 'Unvote' : 'Vote'} {votes}</Button>
+    <IconButton aria-label="voteable" onClick={updateVote}>
+      <>
+        {_vote ? 
+        <ArrowDropUpIcon />
+        : 
+        <ArrowDropUpIcon color="disabled" />
+        }
+        {votes}
+      </>
+    </IconButton>
   )
 }
+// <Button variant={_vote ? "outline-primary" : "primary"} onClick={updateVote}>{_vote ? 'Unvote' : 'Vote'} {votes}</Button>
